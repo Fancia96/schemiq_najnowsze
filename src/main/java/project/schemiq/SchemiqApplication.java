651107fa6b38e5f921eba25ce9640f4f575a1eb9
Fancia <as!@as.pl>
2021-11-25 17:55:22 +0100

    Initial commit

---
+++ src/main/java/project/schemiq/SchemiqApplication.java
@@ -0,0 +1,13 @@
+package project.schemiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/schemiq/SchemiqApplicationTests.java
@@ -0,0 +1,13 @@
+package project.schemiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
